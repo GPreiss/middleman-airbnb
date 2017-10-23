655625bb97473ad13e97e8d6aa090a27c01a80ec
Gabriela Preiss <gabriela.preiss@gmail.com>
2017-10-23 18:18:48 +0100

    Added pages detailing user flats

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["gpreiss", "jolham1985", "anansilva"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
